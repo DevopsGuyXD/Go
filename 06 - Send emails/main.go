08cf7408e2e52e447dfca3790d1948209f091dcb
BharathDundi1 <bharathdundi@gmail.com>
2023-01-30 00:14:59 +0530

    Update main.go

--- 06 - Send emails/main.go
+++ 06 - Send emails/main.go
@@ -2,31 +2,32 @@ package main
 
 import (
 	"crypto/tls"
-	"fmt"
+	"log"
 
 	gomail "gopkg.in/mail.v2"
 )
 
 func main() {
-  m := gomail.NewMessage()
 
-  m.SetHeader("From", "nfcloudsecurity@nowfloats.com")
+	email := gomail.NewMessage()
 
-  m.SetHeader("To", "bharath.dundi@nowfloats.com")
+  email_subject := "AWS securty - Rotate access key"
+  email_body := "Greetings user,\nYou are receiving this email as a gentle reminder to decommission your existing ACCESS KEY in the AWS GETKITSUNE account as it is now older than 90 days. This is currently above the proposed compliance limit.\nWe trust you will do the needful at the earliest."
+  email_from := "nfcloudsecurity@nowfloats.com"
+  email_password := "P67%4urG123"
+  email_to := "bharath.dundi@nowfloats.com"
+  smtp_protocal := "smtp.gmail.com"
+  port := 587
 
-  m.SetHeader("Subject", "Gomail test subject")
+  email.SetHeader("From", email_from)
+  email.SetHeader("To", email_to)
+  email.SetHeader("Subject", email_subject)
+  email.SetBody("text/plain", email_body)
 
-  m.SetBody("text/plain", "This is Gomail test body")
-
-  d := gomail.NewDialer("smtp.gmail.com", 587, "nfcloudsecurity@nowfloats.com", "P67%4urG123")
-
+  d := gomail.NewDialer(smtp_protocal, port, email_from, email_password)
   d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 
-  // Now send E-Mail
-  if err := d.DialAndSend(m); err != nil {
-    fmt.Println(err)
-    panic(err)
+  err := d.DialAndSend(email); if err != nil{
+    log.Fatal(err)
   }
-
-  return
 }
