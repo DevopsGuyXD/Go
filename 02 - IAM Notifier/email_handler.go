06c81801a54180bd08100182e3d507186ae537d9
BharathDundi1 <bharathdundi@gmail.com>
2023-02-01 02:09:41 +0530

    project update

--- 02 - IAM Notifier/email_handler.go
+++ 02 - IAM Notifier/email_handler.go
@@ -12,10 +12,12 @@ func email_handler(user_name string) {
 
 	InitEnvFile()
 
+	aws_secrets := AwsInit()
+
 	email := gomail.NewMessage()
 
-	email_send := os.Getenv("EMAIL_SEND")
-	email_password := os.Getenv("EMAIL_PASSWORD")
+	email_send := aws_secrets["EMAIL_SENDER"]
+	email_password := aws_secrets["EMAIL_SENDER_PASSWORD"]
 	email_subject := "AWS securty - Rotate access key"
 	email_body := "Greetings user,\n\nYou are receiving this email as a gentle reminder to decommission your existing 'ACCESS-KEY' in the 'NowFloats-DEV' account as it is older than 90 days. This is currently above the proposed compliance limit.\n\nWe trust you will do the needful at the earliest."
 	email_connection := os.Getenv("EMAIL_CONNECTION")
