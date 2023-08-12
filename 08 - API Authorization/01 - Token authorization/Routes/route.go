278a83a8c06d8f645e306288cfb1bbb19c736274
BharathDundi1 <bharath.dundi@nowfloats.com>
2023-08-12 15:40:10 +0530

    Adding authorization

---
+++ 08 - API Authorization/01 - Token authorization/Routes/route.go
@@ -0,0 +1,18 @@
+package route
+
+import (
+	"github.com/gorilla/mux"
+
+	controller "github.com/DevopsGuyXD/testserver/Controllers"
+	util "github.com/DevopsGuyXD/testserver/Utils"
+)
+
+func RouteCollection() *mux.Router{
+
+	router := mux.NewRouter()
+
+	router.Handle("/", util.ValidateJWT(controller.HealthController)).Methods("GET")
+	router.HandleFunc("/jwt", util.GetJWT).Methods("GET")
+
+	return router
+}
