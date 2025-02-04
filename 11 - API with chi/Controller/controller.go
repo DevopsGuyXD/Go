dc8a6ec8bf0527e2868b39feabc74bca1c2b2a55
Bharath Dundi <bharathdundi@gmail.com>
2025-02-05 02:14:26 +0530

    Adding chi

--- 11 - API with chi/Controller/controller.go
+++ 11 - API with chi/Controller/controller.go
@@ -8,6 +8,7 @@ import (
 func Test1Controller(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
+	// w.Write([]byte("Working fine - Test 1")) // Use this if response is a non-JSON object
 	json.NewEncoder(w).Encode("Working fine - Test 1")
 }
 
@@ -15,4 +16,17 @@ func Test2Controller(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	json.NewEncoder(w).Encode("Working fine - Test 2")
+}
+
+func TestQueryParameter(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	queryParams := r.URL.Query()
+
+	firstName := queryParams.Get("firstname")
+	lastName := queryParams.Get("lastname")
+
+	fullName := firstName + lastName
+
+	json.NewEncoder(w).Encode(fullName)
 }
--- 11 - API with chi/Routes/routes.go
+++ 11 - API with chi/Routes/routes.go
@@ -9,10 +9,14 @@ func RouteCollection() chi.Router {
 
 	router := chi.NewRouter()
 
+	// Route Collection
 	router.Route("/api", func(r chi.Router) {
 		r.Get("/test1", controller.Test1Controller)
 		r.Get("/test2", controller.Test2Controller)
 	})
 
+	// Query Parameters
+	// http://localhost:8000/api/paramtest?firstname=bharath&lastname=dundi
+	router.Get("/paramtest", controller.TestQueryParameter)
 	return router
 }
