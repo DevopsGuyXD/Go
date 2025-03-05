a06478ec501896ad4b90fe644efa342f1c05b7e5
Bharath Dundi <bharathdundi@gmail.com>
2025-03-05 13:21:52 +0530

    Adding logic for channels

---
+++ 28 - Channels/go.mod
@@ -0,0 +1,3 @@
+module github.com/DevopsGuyXD/Channels
+
+go 1.23.5
--- 28 - Channels/main.go
+++ 28 - Channels/main.go
@@ -2,29 +2,62 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"sync"
+	"time"
 )
 
+// Function to make a simple API request
+func fetchAPI(url string, wg *sync.WaitGroup, ch chan<- string) {
+	defer wg.Done()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprintf("Error: %v", err)
+		return
+	}
+	body, _ := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	ch <- fmt.Sprintf("Concurrent Response Length: %d", len(body))
+}
+
 func main() {
+	urls := []string{
+		"https://jsonplaceholder.typicode.com/todos/1",
+		"https://jsonplaceholder.typicode.com/todos/2",
+		"https://jsonplaceholder.typicode.com/todos/3",
+		"https://jsonplaceholder.typicode.com/todos/4",
+		"https://jsonplaceholder.typicode.com/todos/5",
+	}
 
-	fmt.Println("Welcome to channels")
+	//---------------------------- Sequential Execution
+	start := time.Now()
+	for _, url := range urls {
+		resp, _ := http.Get(url)
+		body, _ := io.ReadAll(resp.Body)
+		resp.Body.Close()
+		fmt.Printf("Sequential Response Length: %d\n", len(body))
+	}
+	fmt.Printf("Sequential Execution Time:%v\n\n", time.Since(start))
 
-	myCh := make(chan int, 1)
-	wg := &sync.WaitGroup{}
+	////---------------------------- Concurrent Execution using Goroutines
+	start = time.Now()
+	var wg sync.WaitGroup
+	ch := make(chan string, len(urls))
 
-	wg.Add(2)
+	for _, url := range urls {
+		wg.Add(1)
+		go fetchAPI(url, &wg, ch)
+	}
 
-		go func(ch <-chan int, wg *sync.WaitGroup){
-			fmt.Println(<- myCh)
-			wg.Done()
-		}(myCh, wg)
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
 
-		go func(ch chan<- int, wg *sync.WaitGroup){
-			myCh <- 5
-			close(myCh)
-			wg.Done()
-		}(myCh, wg)
+	for res := range ch {
+		fmt.Println(res)
+	}
 
-	wg.Wait()
-
+	fmt.Println("Concurrent Execution Time:", time.Since(start))
 }
